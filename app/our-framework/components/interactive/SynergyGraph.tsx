169b01eecb053a6c4e6456f2abf253d465cef2b7
CHINEDU KINGSLEY OKEREKE <Chinedu02k@gmail.com>
2025-01-15 02:56:37 +0000

    chore: remove unused framer-motion import in SynergyGraph component

--- app/our-framework/components/interactive/SynergyGraph.tsx
+++ app/our-framework/components/interactive/SynergyGraph.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import React, { useEffect, useRef } from 'react';
-import { motion } from 'framer-motion';
+// import { motion } from 'framer-motion';
 
 interface PillarNode {
   id: string;
