c7ec8537d79666a1eae8f5898fcf164b3b40e375
Masterchinedum <chinedu02k@gmail.com>
2025-02-02 10:38:13 -0800

    feat: enhance AdminDashboard with OverviewStats, QuickActions, ActivityFeed, and PerformanceMetrics components

--- app/(dashboards)/admindash/page.tsx
+++ app/(dashboards)/admindash/page.tsx
@@ -1,13 +1,18 @@
 import React from 'react';
-import { DashboardLayout } from './components/DashboardLayout';
+import { OverviewStats } from "./components/OverviewStats"
+import { QuickActions } from "./components/QuickActions"
+import { ActivityFeed } from "./components/ActivityFeed"
+import { PerformanceMetrics } from "./components/PerformanceMetrics"
 
-const AdminDashboard = () => {
+export default function AdminDashboard() {
   return (
-    <DashboardLayout>
-      {/* Other components will be added here in subsequent phases */}
-      <div>Dashboard Content Coming Soon</div>
-    </DashboardLayout>
-  );
-};
-
-export default AdminDashboard;
+    <div className="space-y-6">
+      <OverviewStats />
+      <QuickActions />
+      <div className="grid gap-6 md:grid-cols-2">
+        <ActivityFeed />
+        <PerformanceMetrics />
+      </div>
+    </div>
+  )
+}
