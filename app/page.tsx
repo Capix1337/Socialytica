bd224e484d59184b276963b864d06c48bfda1b0f
Masterchinedum <chinedu02k@gmail.com>
2025-03-21 20:50:43 +0100

    fix: ensure RecentBlogPosts component remains commented out in HomePage

--- app/page.tsx
+++ app/page.tsx
@@ -18,7 +18,7 @@ export default function HomePage() {
       <FeaturesGrid />
       <HowItWorks />
       <CallToAction />
-      {/* <RecentBlogPosts /> */}
+      {/* <RecentBlogPosts /> */} 
       <Newsletter />
     </main>
   )
