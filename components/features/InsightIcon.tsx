37d29c996664d6fed6ab0dd7539d15de3d59fed6
Masterchinedum <chinedu02k@gmail.com>
2025-03-25 15:58:57 +0100

    feat: add InsightIcon component for enhanced visual representation

---
+++ components/features/InsightIcon.tsx
@@ -0,0 +1,84 @@
+export default function InsightIcon() {
+  return (
+    <svg
+      width="51"
+      height="51"
+      viewBox="0 0 51 51"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      className="w-[51px] h-[51px] relative"
+    >
+      <g clipPath="url(#clip0_4741_8799)">
+        <path
+          d="M31.6465 38.2437C31.6465 30.7458 38.1196 30.1731 38.2755 22.6752C38.4222 15.8015 32.8474 10.8855 25.4849 10.8855C18.1133 10.8855 12.5479 15.8015 12.6854 22.6752C12.8413 30.1731 19.3144 30.7458 19.3144 38.2437"
+          stroke="#243757"
+          strokeWidth="2"
+          strokeMiterlimit="20"
+        />
+        <path
+          d="M38.855 4.14136L34.5029 10.1284"
+          stroke="#FC8E77"
+          strokeWidth="2"
+          strokeMiterlimit="2.2561"
+        />
+        <path
+          d="M40.6533 17.4777L47.4687 15.1919"
+          stroke="#FC8E77"
+          strokeWidth="2"
+          strokeMiterlimit="2.2561"
+        />
+        <path
+          d="M47.4602 29.9057L40.6387 27.6199"
+          stroke="#FC8E77"
+          strokeWidth="2"
+          strokeMiterlimit="2.2561"
+        />
+        <path
+          d="M3.51581 29.9057L10.3155 27.6277"
+          stroke="#FC8E77"
+          strokeWidth="2"
+          strokeMiterlimit="2.2561"
+        />
+        <path
+          d="M10.3354 17.4777L3.51953 15.1919"
+          stroke="#FC8E77"
+          strokeWidth="2"
+          strokeMiterlimit="2.2561"
+        />
+        <path
+          d="M12.0996 4.12622L16.459 10.1284"
+          stroke="#FC8E77"
+          strokeWidth="2"
+          strokeMiterlimit="2.2561"
+        />
+        <path
+          d="M25.4813 0.455566V7.82735"
+          stroke="#FC8E77"
+          strokeWidth="2"
+          strokeMiterlimit="2.2561"
+        />
+        <path
+          d="M19.506 22.9604L25.499 26.7306M25.499 26.7306V41.439M25.499 26.7306L31.4434 22.9604"
+          stroke="#FC8E77"
+          strokeWidth="2"
+          strokeMiterlimit="2.2561"
+          strokeLinecap="round"
+        />
+        <path
+          fillRule="evenodd"
+          clipRule="evenodd"
+          d="M31.6011 41.9771H19.3535V45.4444C19.3535 46.39 19.7307 47.2995 20.4001 47.9661C21.0694 48.6327 21.9738 49.0099 22.9209 49.0099H28.0337C28.9808 49.0099 29.8852 48.6327 30.5545 47.9661C31.2239 47.2995 31.6011 46.39 31.6011 45.4444V41.9771Z"
+          stroke="#243757"
+          strokeWidth="2"
+          strokeMiterlimit="2.2561"
+          strokeLinecap="round"
+        />
+      </g>
+      <defs>
+        <clipPath id="clip0_4741_8799">
+          <rect width="50" height="50" fill="white" transform="translate(0.5 0.438965)" />
+        </clipPath>
+      </defs>
+    </svg>
+  );
+}
